7033c62be5a427e8e4a956712abc9141a5c59890
Ambatucode <christianmolina1669@gmail.com>
2026-08-24 16:41:56 +0800

    Bump version to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
