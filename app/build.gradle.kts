c6021032dc1854251a0f4c8a03480d0b82ce20e5
Ambatucode <christianmolina1669@gmail.com>
2026-08-24 19:47:33 +0800

    Bump version to 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
