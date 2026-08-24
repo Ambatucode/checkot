7009c0bb4b419aebf37e4ad2e0da3672815b3cfd
Ambatucode <christianmolina1669@gmail.com>
2026-08-24 13:50:20 +0800

    Fix: Append MAPS_API_KEY right before clean assembleDebug and bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
