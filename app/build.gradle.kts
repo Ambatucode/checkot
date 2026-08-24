5a2e1deabb138dce8d14d09744c0b2a34a170911
Ambatucode <christianmolina1669@gmail.com>
2026-08-24 19:29:35 +0800

    Bump version to 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
