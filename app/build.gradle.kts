ce3cbb99ba2b59df8483610451e8c641feec7228
Ambatucode <christianmolina1669@gmail.com>
2026-08-24 12:21:21 +0800

    Feat: Lock orientation to portrait and bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
