4bcdc8f4ccff8022e48db064e948463f1dbff86a
Ambatucode <christianmolina1669@gmail.com>
2026-08-25 14:16:37 +0800

    Bump version to 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "2.3"
+        versionCode = 15
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
