3cfed587d81b72cca339618ffa4f7020b4c3715e
Ambatucode <christianmolina1669@gmail.com>
2026-08-25 16:19:47 +0800

    Bump version to 2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.8"
+        versionCode = 20
+        versionName = "2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
