7aa88462a6df17bb441936411b2df5aeace2cf68
Ambatucode <christianmolina1669@gmail.com>
2026-08-26 17:17:48 +0800

    Bump version to 4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "3.9"
+        versionCode = 31
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
