4190308a76dfd7ef04875a9dc8f556e1a87bcbd9
Ambatucode <christianmolina1669@gmail.com>
2026-09-06 15:58:20 +0800

    chore: bump versionCode to 42 and versionName to 5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.app.checkot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 41
-        versionName = "5.0"
+        versionCode = 42
+        versionName = "5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
